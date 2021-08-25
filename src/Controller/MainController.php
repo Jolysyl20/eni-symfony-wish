f66c610991be5f1460f13155712a8b954dfa0638
S33787 <jolysyl20@hotmail.com>
2021-08-25 17:07:02 +0200

    Formulaire
    mise en place d'un formulaire  d'ajout d'un wish
    
    mise en place d'un css bootstrap

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Wish;
+use App\Form\WishType;
 
 class MainController extends AbstractController
 {
@@ -29,15 +32,53 @@ class MainController extends AbstractController
         ]);
     }
     /**
-     * @Route("/wish", name="wish_liste")
+     * @Route("/wish", name="wish")
      */
     public function wish(WishRepository $repo): Response
     {
-        $tab = $repo->findAll();
+        $wishes = $repo->findBy(['isPublished' => true], ['dateCreated'=>'DESC']);
 
         return $this->render('main/wish.html.twig', [
-            'wish' => $tab,
+            'wishes' => $wishes
         ]);
+    }
+    /**
+     * @Route("/ajouter", name="ajouter")
+     */
+    public function ajouter(EntityManagerInterface $em): Response
+    {
+       
+        $wish = new Wish();
+        $wish ->setTitle('apprendre');
+        $wish ->setDescription('apprendre a developper');
+        $wish ->setAuthor('sylvain');
+        $wish ->setIsPublished(True);
+        $wish ->setDateCreated(new \DateTime());
+        $em->persist($wish);
+        $em->flush();
+        return $this->redirect('wish');
+    }
+
+      /**
+     * @Route("/add", name="add")
+     */
+    public function add(EntityManagerInterface $em): Response
+    {
+        $wish = new Wish();
+        $formWish = $this->createForm(WishType::class,$wish);
+        //création d'un objetformulaire associé l'entité $wish
+      /*  $wish = new Wish();
+        $wish ->setTitle('apprendre');
+        $wish ->setDescription('apprendre a developper');
+        $wish ->setAuthor('sylvain');
+        $wish ->setIsPublished(True);
+        $wish ->setDateCreated(new \DateTime());
+        $em->persist($wish);
+        $em->flush();*/
+        return $this->render('main/ajouter.html.twig',[
+            'formWish' => $formWish->createView()
+        ]);
+
     }
 
     /**
